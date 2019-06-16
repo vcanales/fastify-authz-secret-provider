056e90ef1389102464571c1270327d2b51e1e2cf
Vicente Canales <vicente@canales.io>
2019-06-16 17:48:23 -0400

    feat: add fastify secret provider

---
+++ index.js
@@ -0,0 +1,38 @@
+import JwksClient, { ArgumentError } from 'jwks-rsa';
+
+const handleSigningKeyError = (err, cb) => {
+  // If we didn't find a match, can't provide a key.
+  if (err && err.name === 'SigningKeyNotFoundError') {
+    return cb(null);
+  }
+  return cb(err);
+};
+
+export const fastifyJwtSecret = options => {
+  if (options === null || options === undefined) {
+    throw new ArgumentError(
+      'An options object must be provided when initializing fastifyJwtSecret',
+    );
+  }
+
+  const client = new JwksClient(options);
+  const onError = options.handleSigningKeyError || handleSigningKeyError;
+  return function secretProvider(request, decoded, cb) {
+    // if decoded is null, token is not present or is invalid
+    if (!decoded) {
+      return cb(new Error('Invalid token'), null);
+    }
+    // Only RS256 is supported.
+    if (decoded.header.alg !== 'RS256') {
+      return cb(new Error('Only RS256 is supported'), null);
+    }
+
+    client.getSigningKey(decoded.header.kid, (err, key) => {
+      if (err) {
+        return onError(err, newError => cb(newError, null));
+      }
+      // Provide the key.
+      return cb(null, key.publicKey || key.rsaPublicKey);
+    });
+  };
+};
---
+++ test/index.test.js
@@ -0,0 +1,91 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import { ArgumentError } from 'jwks-rsa';
+import Fastify from 'fastify';
+import fastifyJwt from 'fastify-jwt';
+import { fastifyJwtSecret } from '../';
+
+describe('fastifyJwtSecret', () => {
+  it('should throw error if options.jwksUri is null', () => {
+    let err = null;
+
+    try {
+      new fastifyJwtSecret();
+    } catch (e) {
+      err = e;
+    }
+
+    expect(err instanceof ArgumentError).to.be.true;
+  });
+
+  it('should throw an error if token is invalid', done => {
+    const fastify = Fastify();
+
+    fastify.register(fastifyJwt, {
+      secret: fastifyJwtSecret({
+        jwksUri: 'http://localhost/.well-known/jwks.json',
+      }),
+      algorithms: ['RS256'],
+      decode: { complete: true },
+    });
+
+    fastify.get('/auth-required', (request, reply) => {
+      request
+        .jwtVerify()
+        .then(decoded => reply.send(decoded))
+        .catch(err => reply.send(err));
+    });
+
+    fastify.listen().then(address => {
+      request(address)
+        .get('/auth-required')
+        .set('Authorization', 'Bearer abc')
+        .expect(401)
+        .end((_, res) => {
+          expect(JSON.parse(res.text).message).to.equal('Invalid token');
+          done();
+        });
+    });
+  });
+  it('should throw an error if algorithm is not RS256', done => {
+    const fastify = Fastify();
+    // token signed with HS256 alg
+    // header:
+    /**
+      {
+        "alg": "HS256",
+        "typ": "JWT"
+      }
+    */
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N_XgL0n3I9PlFUP0THsR8U';
+
+    fastify.register(fastifyJwt, {
+      secret: fastifyJwtSecret({
+        jwksUri: 'http://localhost/.well-known/jwks.json',
+      }),
+      algorithms: ['HS256'],
+      decode: { complete: true },
+    });
+
+    fastify.get('/auth-required', (request, reply) => {
+      request
+        .jwtVerify()
+        .then(decoded => reply.send(decoded))
+        .catch(err => reply.send(err));
+    });
+
+    fastify.listen().then(address => {
+      request(address)
+        .get('/auth-required')
+        .set('authorization', `Bearer ${token}`)
+        .expect(401)
+        .end((err, res) => {
+          expect(JSON.parse(res.text).message).to.equal(
+            'Only RS256 is supported',
+          );
+          done();
+        });
+    });
+  });
+});
